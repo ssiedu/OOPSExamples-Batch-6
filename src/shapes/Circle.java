4051c5f4ed7e176a08664b3de4d45656bef5b280
manoj <manojs121@rediffmail.com>
2021-04-14 19:03:26 +0530

    140421

--- src/shapes/Circle.java
+++ src/shapes/Circle.java
@@ -5,17 +5,18 @@ public class Circle {
 
 	//data-members (variables for storing the property values of an object)
 	private double radius, area, circumference;
+	private static double PI=3.14;
 	
 	//methods (to perform operations on an object)
 	public void setRadius(double r) {
 		radius=r;
 	}
 	public void computeArea() {
-		area = 3.14*radius*radius;
+		area = PI*radius*radius;
 		System.out.println("Area of Circle  : "+area);
 	}
 	public void computeCircumference() {
-		circumference=2*3.14*radius;
+		circumference=2*PI*radius;
 		System.out.println("Circumference of Circle : "+circumference);
 	}
 	
