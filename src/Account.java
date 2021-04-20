5f0977095495293f744c91b7e5718a2875e0ab72
manoj <manojs121@rediffmail.com>
2021-04-20 18:09:51 +0530

    200421

--- src/Account.java
+++ src/Account.java
@@ -5,6 +5,21 @@ public class Account {
 	private String name;
 	private int balance;
 	private static int totalBalance;
+
+	public static void showAvg() {
+		System.out.println("Avg of 0 : 0");
+	}
+	public static void showAvg(Account tmp1, Account tmp2, Account tmp3, Account tmp4) {
+		System.out.println("Avg of 4 : "+(tmp1.balance+tmp2.balance+tmp3.balance+tmp4.balance)/4);
+	}
+	
+	public static void showAvg(Account tmp1, Account tmp2, Account tmp3) {
+		System.out.println("Avg of 3 : "+(tmp1.balance+tmp2.balance+tmp3.balance)/3);
+	}
+	
+	public static void showAvg(Account tmp1, Account tmp2) {
+		System.out.println("Avg of 2 : "+(tmp1.balance+tmp2.balance)/2);
+	}
 	
 	public void showBalance() {
 		System.out.println("Balance in "+ano+" is "+balance);
--- src/BankingApp.java
+++ src/BankingApp.java
@@ -6,10 +6,24 @@ public class BankingApp {
 		Account ac1=new Account();
 		Account ac2=new Account();
 		Account ac3=new Account();
+		Account ac4=new Account();
 		
 		ac1.setData(111, "AAA", 10000);
 		ac2.setData(112, "BBB", 20000);
 		ac3.setData(113, "CCC", 30000);
+		ac4.setData(114, "DDD", 40000);
+		ac1.showBalance();
+		ac2.showBalance();
+		ac3.showBalance();
+		ac4.showBalance();
+		
+		Account.showAvg();
+		Account.showAvg(ac1,ac2);
+		Account.showAvg(ac1,ac2,ac3);
+		Account.showAvg(ac1,ac2,ac3,ac4);
+		
+		//ac1.showAvg(ac2);
+		//ac2.showAvg(ac3);
 		
 		/*
 		Account.showBalance(ac1);
@@ -17,9 +31,7 @@ public class BankingApp {
 		Account.showBalance(ac3);
 		*/
 		
-		ac1.showBalance();
-		ac2.showBalance();
-		ac3.showBalance();
+		
 		
 		/*
 		ac1.showData();
